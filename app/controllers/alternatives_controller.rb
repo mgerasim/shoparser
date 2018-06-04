9a0d9d57b79bd2c200b085d7f0435eab023834bb
Mikhail Gerasimov <mgerasim@inbox.ru>
2018-06-04 10:27:41 +1000

    Добавить кирилллицу в нименование форм и элементов управления #3

--- app/controllers/alternatives_controller.rb
+++ app/controllers/alternatives_controller.rb
@@ -28,7 +28,7 @@ class AlternativesController < ApplicationController
 
     respond_to do |format|
       if @alternative.save
-        format.html { redirect_to @alternative, notice: 'Alternative was successfully created.' }
+        format.html { redirect_to @alternative, notice: 'Аналог успешно создан.' }
         format.json { render :show, status: :created, location: @alternative }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class AlternativesController < ApplicationController
   def update
     respond_to do |format|
       if @alternative.update(alternative_params)
-        format.html { redirect_to @alternative, notice: 'Alternative was successfully updated.' }
+        format.html { redirect_to @alternative, notice: 'Аналог успешно обновлен.' }
         format.json { render :show, status: :ok, location: @alternative }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class AlternativesController < ApplicationController
   def destroy
     @alternative.destroy
     respond_to do |format|
-      format.html { redirect_to alternatives_url, notice: 'Alternative was successfully destroyed.' }
+      format.html { redirect_to alternatives_url, notice: 'Аналог успешно удален.' }
       format.json { head :no_content }
     end
   end
--- app/controllers/tovars_controller.rb
+++ app/controllers/tovars_controller.rb
@@ -28,7 +28,7 @@ class TovarsController < ApplicationController
 
     respond_to do |format|
       if @tovar.save
-        format.html { redirect_to @tovar, notice: 'Tovar was successfully created.' }
+        format.html { redirect_to @tovar, notice: 'Товар успешно создан.' }
         format.json { render :show, status: :created, location: @tovar }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class TovarsController < ApplicationController
   def update
     respond_to do |format|
       if @tovar.update(tovar_params)
-        format.html { redirect_to @tovar, notice: 'Tovar was successfully updated.' }
+        format.html { redirect_to @tovar, notice: 'Товар успешно обновлен.' }
         format.json { render :show, status: :ok, location: @tovar }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class TovarsController < ApplicationController
   def destroy
     @tovar.destroy
     respond_to do |format|
-      format.html { redirect_to tovars_url, notice: 'Tovar was successfully destroyed.' }
+      format.html { redirect_to tovars_url, notice: 'Товар успешно удален.' }
       format.json { head :no_content }
     end
   end
